1b1498c32e39aa2a0b960313926f1d6547dc4664
Augusto Soares <gutosoares@outlook.com>
2020-02-08 21:22:46 -0300

    Config main App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Routes from './routes'
+import { makeStyles } from '@material-ui/core'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+    backgroundColor: '#121212',
+  },
+}))
 
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <Header />
+        <Container
+          component="main"
+          className={classes.main}>
+          <Routes />
+        </Container>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
