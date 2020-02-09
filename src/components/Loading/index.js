6a9c8cf4826d05e76f77498d704344a9e03e230d
Augusto Soares <gutosoares@outlook.com>
2020-02-08 21:34:28 -0300

    Create loading components

---
+++ src/components/Loading/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactLoading from 'react-loading'
+
+import './styles.css'
+
+function Loading() {
+  return (
+    <div id="loading">
+      <ReactLoading className="loading" type="spin" color="#000" />
+    </div>
+  )
+} 
+
+export default Loading
+
+
