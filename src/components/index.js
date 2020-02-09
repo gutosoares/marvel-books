72407ff0646a3ba37d89db082638ec411fe6150e
Augusto Soares <gutosoares@outlook.com>
2020-02-08 21:34:58 -0300

    Config import and exports helpers components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Input from './Input'
+import Header from './Header'
+import Footer from './Footer'
+import Loading from './Loading'
+
+export { 
+  Input,
+  Header,
+  Footer,
+  Loading
+}
