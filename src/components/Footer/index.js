46d80f94f96f77e0f752117000baf8c3d15a5e6d
Augusto Soares <gutosoares@outlook.com>
2020-02-08 21:36:20 -0300

    Create footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Container, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor: '#1b1b1b',
+    color: '#eee',
+  },
+  copyright: {
+    color: '#eee',
+  }
+}))
+
+function Copyright() {
+  return (
+    <Typography variant="body2" align="center" color="inherit">
+      {'Copyright © '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+function Footer() {
+  const classes = useStyles()
+
+  return (
+    <footer className={classes.footer}>
+      <Container maxWidth="sm">
+        <Typography variant="body1" align="center">
+          Marvel Books
+        </Typography>
+        <Copyright />
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
