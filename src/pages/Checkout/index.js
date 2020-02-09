5436930b678cfdc1337237e5fd5ea814987198be
Augusto Soares <gutosoares@outlook.com>
2020-02-09 08:31:04 -0300

    Create checkout page

---
+++ src/pages/Checkout/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import { Container } from '@material-ui/core'
+
+function Checkout() {
+  return (
+    <Fragment>
+      <Container maxWidth="lg">
+        <h1>Checkout</h1>
+      </Container>
+    </Fragment>
+  )
+}
+
+export default Checkout
